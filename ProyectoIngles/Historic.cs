4bc7944928a0adf022964c42c9233d8dabdff6ef
poquet <dpoqram@epsg.upv.es>
2021-05-03 23:05:44 +0200

    Second commit update

---
+++ ProyectoIngles/Historic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoIngles
+{
+
+    public class Historic
+    {
+        public int id { get; set; }
+        public int DoctorId { get; set; }
+        public int PacientId { get; set; }
+
+        public string diagnose { get; set; }
+        public string treatment { get; set; }
+        public string date { get; set; }
+
+        public Historic(int id, int doctorId, int pacientId, string diagnose, string treatment, string date)
+        {
+            this.id = id;
+            DoctorId = doctorId;
+            PacientId = pacientId;
+            this.diagnose = diagnose;
+            this.treatment = treatment;
+            this.date = date;
+        }
+
+        public Historic() { }
+    }
+}
--- WebApplication/Home.aspx.cs
+++ WebApplication/Home.aspx.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using WebApplication.webServiceReference;
 namespace WebApplication
 {
     public partial class Home : System.Web.UI.Page
-    {
+    {     
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            WebService1 ws = new WebService1();
         }
     }
 }
